ea06331078d014daee842f43163e0b67163c8123
harrislb <lexibharris@yahoo.com>
2015-04-17 21:13:40 -0400

    Finished Task 3

--- src/ExpediaTest/UserTest.java
+++ src/ExpediaTest/UserTest.java
@@ -99,6 +99,23 @@ public class UserTest
 		target.bookWithDoubleMiles(new Booking[]{flight});
 		Assert.assertEquals(5000, target.bonusFrequentFlierMiles, 0.01);
 	}
+	
+	@Test 
+	public void TestDoubleMilesForLower()
+	{
+		Flight flight = new Flight(StartDate,EndDate,300);
+		target.bookWithDoubleMiles(new Booking[]{flight});
+		Assert.assertEquals(600,target.bonusFrequentFlierMiles,0.01);
+	}
+	
+	@Test
+	public void TestThatDiscountInitializes()
+	{
+	Discount target = new Discount(0.01, 1);
+	ServiceLocator.Instance().AddDiscount(target);
+	this.target.book(new Booking[]{new Flight(StartDate, EndDate, 100), new Hotel(5), new Car(3)});
+	assertEquals(1024.65,this.target.Price(), 0.01);
+	}
 
 	
 	@After
